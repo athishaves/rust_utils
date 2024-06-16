4d0ceb285dbce04b84ccf62524b851eb7cbadc11
Athish Venkatesh <athishaves@gmail.com>
2024-06-16 21:19:52 +0530

    add auto run shell script

---
+++ run_rust_project.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH=/home/ec2-user/.nvm/versions/node/v20.11.1/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/bin
+
+cd /home/ec2-user/rust_utils
+./target/debug/news_scraper
